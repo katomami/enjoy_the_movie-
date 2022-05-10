72e2eb7a3592c744ee321c15968dd21c3ca7b6e2
katomami <alpaca080013@gmail.com>
2022-05-10 19:15:28 +0000

    [Update]

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
-  devise_for :users,skip: [:passwords,], controllers: {
-  registrations: "users/registrations",
-  sessions: 'users/sessions'
+  devise_for :user,skip: [:passwords,], controllers: {
+  registrations: "user/registrations",
+  sessions: 'user/sessions'
 }
+
+root to: 'user/homes#top'
+get 'about' => 'user/homes#about'
+
+namespace :admin do
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
