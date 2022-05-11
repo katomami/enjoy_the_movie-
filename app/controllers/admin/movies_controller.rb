3c84fba253e0a82023293a0ca3e55855f5e0ef80
katomami <alpaca080013@gmail.com>
2022-05-11 16:27:41 +0000

    [Update]

---
+++ app/controllers/admin/movies_controller.rb
@@ -0,0 +1,28 @@
+class Admin::MoviesController < ApplicationController
+  def index
+    @movies = Movie.all  
+  end
+  def new
+    @movie = Movie.new
+  end
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to admin_movies_path
+  end
+  def show
+    @movie = Movie.find(params[:id])
+  end
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+  def update
+    movie = Movie.find(params[:id])
+    movie.update(movie_params)
+    redirect_to admin_movies_path
+  end
+  private
+  def movie_params
+    params.require(:movie).permit(:genre_id,:title,:director_name,:production_year,:explanation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,11 @@ Rails.application.routes.draw do
 
 root to: 'user/homes#top'
 get 'about' => 'user/homes#about'
-
+get 'movies/search' => 'user/movies#search'
+get 'movies' => 'user/movies#index'
 namespace :admin do
+  resources :movies
+  resources :genres
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
